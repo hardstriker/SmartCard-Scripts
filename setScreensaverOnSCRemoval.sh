43854b34b0624df1825df771dc4bdd3afbe5ca09
hardstriker <woodyhome@gmail.com>
2018-01-23 18:28:32 -0700

    Update and rename setScreensaverOnSCRemoval to setScreensaverOnSCRemoval.sh

--- setScreensaverOnSCRemoval.sh
+++ setScreensaverOnSCRemoval.sh
@@ -4,6 +4,17 @@
 # Created by Matt Woodruff, Sr Systems Engineer @ Jamf
 # January 23, 2018
 ######
+#
+######
+# INSTRUCTIONS
+# Setup a new Jamf Pro Policy with the following settings
+#
+# Trigger: Login (verify Login hooks are enabled within the Settings -> Management Framework -> Checkin -> Login/Logout Hooks
+# Frequency: Ongoing
+# Make available Offline
+#
+# Used in Mac OS X 10.12 and higher
+######
 
 curUser=$(ls -l /dev/console | cut -d " " -f 4)
 
